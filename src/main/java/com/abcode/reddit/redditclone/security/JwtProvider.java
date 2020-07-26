8358bd9ea8e2e5f889da2321aa610757dead613e
Arthur Barbosa <arthurbruno03@gmail.com>
2020-07-26 17:51:08 -0400

    [added] added valid JKS file

--- src/main/java/com/abcode/reddit/redditclone/security/JwtProvider.java
+++ src/main/java/com/abcode/reddit/redditclone/security/JwtProvider.java
@@ -26,24 +26,23 @@ public class JwtProvider {
     @Value("${jwt.expiration.time}")
     private Long jwtExpirationInMillis;
 
-//    @PostConstruct
-//    public void init() {
-//        try {
-//            keyStore = KeyStore.getInstance("JKS");
-//            InputStream resourceAsStream = getClass().getResourceAsStream("/springblog.jks");
-//            keyStore.load(resourceAsStream, "secret".toCharArray());
-//        } catch (KeyStoreException | CertificateException | NoSuchAlgorithmException | IOException e) {
-//            throw new RedditException("Exception occurred while loading keystore", e);
-//        }
-//
-//    }
+    @PostConstruct
+    public void init() {
+        try {
+            keyStore = KeyStore.getInstance("JKS");
+            InputStream resourceAsStream = getClass().getResourceAsStream("/springblog.jks");
+            keyStore.load(resourceAsStream, "secret".toCharArray());
+        } catch (KeyStoreException | CertificateException | NoSuchAlgorithmException | IOException e) {
+            throw new RedditException("Exception occurred while loading keystore", e);
+        }
+    }
 
     public String generateToken(Authentication authentication) {
         User principal = (User) authentication.getPrincipal();
         return Jwts.builder()
                 .setSubject(principal.getUsername())
                 .setIssuedAt(from(Instant.now()))
-                //.signWith(getPrivateKey())
+                .signWith(getPrivateKey())
                 .setExpiration(Date.from(Instant.now().plusMillis(jwtExpirationInMillis)))
                 .compact();
     }
@@ -52,18 +51,18 @@ public class JwtProvider {
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(from(Instant.now()))
-                //.signWith(getPrivateKey())
+                .signWith(getPrivateKey())
                 .setExpiration(Date.from(Instant.now().plusMillis(jwtExpirationInMillis)))
                 .compact();
     }
 
-//    private PrivateKey getPrivateKey() {
-//        try {
-//            return (PrivateKey) keyStore.getKey("springblog", "secret".toCharArray());
-//        } catch (KeyStoreException | NoSuchAlgorithmException | UnrecoverableKeyException e) {
-//            throw new RedditException("Exception occured while retrieving public key from keystore", e);
-//        }
-//    }
+    private PrivateKey getPrivateKey() {
+        try {
+            return (PrivateKey) keyStore.getKey("springblog", "secret".toCharArray());
+        } catch (KeyStoreException | NoSuchAlgorithmException | UnrecoverableKeyException e) {
+            throw new RedditException("Exception occured while retrieving public key from keystore", e);
+        }
+    }
 
     public boolean validateToken(String jwt) {
         parser().setSigningKey(getPublickey()).parseClaimsJws(jwt);
